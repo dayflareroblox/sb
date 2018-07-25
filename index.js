58a62916504e3699777b6b7781453d278f018c73
discordmania <41619492+discordmania@users.noreply.github.com>
2018-07-25 11:28:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ bot.on("message", async message => {
   let args = messageArray.slice(1);
 
   if(cmd === `${prefix}verify`){
-    message.member.addRole(member.guild.roles.find("name", "Verified member"));
+    message.member.addRole(message.guild.roles.find("name", "Verified member"));
     message.channel.send("Your all ranked up! :smile");
   }
   

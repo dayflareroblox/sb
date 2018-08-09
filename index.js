fd11705194b64c9448892e13177d79ec3305c29e
discordmania <41619492+discordmania@users.noreply.github.com>
2018-08-09 19:24:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ if(cmd === `${prefix}info1`){
      .setColor("#62a4f5")
      .setThumbnail("https://cdn.discordapp.com/attachments/471654505830940672/471654608511434752/skylounge.png")
      message.channel.send(aembed);
-     message.channel.send("<@&477178469135024150>")
+     message.channel.send("<@&477180353786937355>")
      message.delete();
   }
 

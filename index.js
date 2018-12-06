85344e66f5fb44d4b09a9e7cbba1e59dbaac910a
discordmania <41619492+discordmania@users.noreply.github.com>
2018-12-06 17:03:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ if(cmd === `${prefix}purge`){
 
 }
 var servers = {};
-var prefix = '!;
+var prefix = '!';
 client.on("message", async message => {
     var args = message.content.substring(prefix.length).split(" ");
     if (!message.content.startsWith(prefix)) return;
